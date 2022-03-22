4f6e86b2bce9f242782d919c7f90b61f83b38786
Muhammed Eren Demir <mhmmderen3@gmail.com>
2022-03-22 13:54:20 +0300

    edit controller

--- src/main/java/com/muhammederendemir/springbootweatherapi/controller/WeatherController.java
+++ src/main/java/com/muhammederendemir/springbootweatherapi/controller/WeatherController.java
@@ -1,12 +1,9 @@
 package com.muhammederendemir.springbootweatherapi.controller;
 
 import com.muhammederendemir.springbootweatherapi.dto.WeatherDTO;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Locale;
 
 @RestController
 public class WeatherController {
