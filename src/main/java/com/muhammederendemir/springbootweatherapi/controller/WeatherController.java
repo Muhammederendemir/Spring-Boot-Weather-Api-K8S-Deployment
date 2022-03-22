3dd8043658ce129e4a13954781b3065d1356c228
Muhammed Eren Demir <mhmmderen3@gmail.com>
2022-03-22 14:24:35 +0300

    edit Jenkinsfile

--- src/main/java/com/muhammederendemir/springbootweatherapi/controller/WeatherController.java
+++ src/main/java/com/muhammederendemir/springbootweatherapi/controller/WeatherController.java
@@ -29,7 +29,7 @@ public class WeatherController {
     @GetMapping(value = "/")
     public String getWelcomePage() {
 
-        return "Detaysoft";
+        return "Çalık Holding";
     }
 
     @GetMapping(value = "/temperature")
